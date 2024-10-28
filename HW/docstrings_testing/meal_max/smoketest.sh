5c42e985b5502090fba196a035ec63358fffd2d0
MZdank <MZdank@bu.edu>
2024-10-28 14:04:50 -0400

    added mealmax

---
+++ HW/docstrings_testing/meal_max/smoketest.sh
@@ -0,0 +1,31 @@
+# Should be the same????
+BASE_URL="http://localhost:5000/api"
+
+# Flag to control whether to echo JSON output
+ECHO_JSON=flase
+
+check_health() {
+  echo "Checking health status..."
+  curl -s -X GET "$BASE_URL/health" | grep -q '"status": "healthy"'
+  if [ $? -eq 0 ]; then
+    echo "Service is healthy."
+  else
+    echo "Health check failed."
+    exit 1
+  fi
+}
+
+check_db() {
+  echo "Checking database connection..."
+  curl -s -X GET "$BASE_URL/db-check" | grep -q '"database_status": "healthy"'
+  if [ $? -eq 0 ]; then
+    echo "Database connection is healthy."
+  else
+    echo "Database check failed. :("
+    exit 1
+  fi
+}
+
+check_health
+
+check_db
